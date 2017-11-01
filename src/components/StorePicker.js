c5e43db2f5d616a9229ed937a11f386c79e803c5
himanish58 <himanish.kabiraj@gmail.com>
2017-11-02 04:06:00 +0530

    Added event handler to StorePicker and bind the onclick function to the component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log(this.StoreInput.value);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required />
+        <input type="text" placeholder="Store Name" defaultValue={getFunName()}
+          ref={(input) => {this.StoreInput = input;}} required />
+
         <button type="submit">Visit Store &rarr;</button>
       </form>
     );
