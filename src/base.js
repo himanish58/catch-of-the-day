f398d17f44d44b354b28fad77b59079bea788c25
himanish58 <himanish.kabiraj@gmail.com>
2017-11-10 00:08:56 +0530

    Added firebase to maintain fishes state

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyAa4QsyD8SFPweN8_0dmqPhmIHkGl9kjBo',
+  authDomain: 'catch-of-the-day-6685d.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-6685d.firebaseio.com',
+  projectId: 'catch-of-the-day-6685d',
+});
+
+export default base;
